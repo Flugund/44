ba840ccbf8872fd15fcff444009cb558760d357b
Sam Nydahl <sam.nydahl@smb.nu>
2016-11-28 22:45:42 +0100

    Extracted map settings.

---
+++ resources/assets/js/components/Signup.vue
@@ -0,0 +1,25 @@
+<style lang="sass" scoped>
+
+</style>
+
+<template>
+    <div>
+        signup
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        },
+
+        ready() {
+
+        },
+
+        methods: {
+
+        }
+    }
+</script>
