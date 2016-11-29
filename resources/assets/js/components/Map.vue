ec1c56155304c203ca1e61893a79777cb602dd42
Sam Nydahl <sam.nydahl@smb.nu>
2016-11-29 01:49:08 +0100

    Faked coordinates to correspond with dummy-data in db.

--- resources/assets/js/components/Map.vue
+++ resources/assets/js/components/Map.vue
@@ -20,34 +20,8 @@
     import MapSettings from './MapSettings';
 
     export default {
-        route: {
-            data(transition) {
-                // fetch data-points
-                // var apiData = [
-                //     {long: 40.730610, lat: -73.935242},
-                //     {long: 40.730610, lat: -73.945242},
-                //     {long: 40.730610, lat: -73.955242},
-                //     {long: 40.730610, lat: -73.965242},
-
-                //     {long: 40.750610, lat: -73.975242},
-                //     {long: 40.750610, lat: -73.975242},
-                //     {long: 40.750610, lat: -73.975242},
-                //     {long: 40.750610, lat: -73.975242},
-                // ];
-
-                // _.each(apiData, (item) => {
-                //     this.heatmapData.push(new google.maps.LatLng(item.long, item.lat));
-                // });
-
-                transition.next(transition);
-            }
-        },
-
         data() {
-            return {
-                heatmapData: [],
-                map: {}
-            };
+            return {};
         },
 
         ready() {
@@ -59,16 +33,20 @@
 
                 Locator.getLocation((coordinates) => {
                     var styledMapType = new google.maps.StyledMapType(MapSettings);
-                    console.log(coordinates.lat, coordinates.lng);
+
+                    // Fake location to NYC
+                    coordinates.lng = -74.1344;
+                    coordinates.lat = 40.6349;
+
                     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-                      center: new google.maps.LatLng(coordinates.lat, coordinates.lng),
-                      zoom: 13,
-                      mapTypeId: 'styled_map',
-                      disableDefaultUI: true
+                        center: new google.maps.LatLng(coordinates.lat, coordinates.lng),
+                        zoom: 13,
+                        mapTypeId: 'styled_map',
+                        disableDefaultUI: true
                     });
 
                     var heatmap = new google.maps.visualization.HeatmapLayer({
-                      data: this.heatmapData
+                      data: []
                     });
                     heatmap.setMap(map);
                     map.mapTypes.set('styled_map', styledMapType);
@@ -78,24 +56,30 @@
                         var ne = bb.getNorthEast(); // top-left
                         var sw = bb.getSouthWest(); // bottom-right
 
-                        this.fetchDataPoints(sw.lng(), ne.lat(), ne.lng(), sw.lat());
+                        this.fetchDataPoints(heatmap, sw.lng(), ne.lat(), ne.lng(), sw.lat());
                     });
                 }, (errorMsg) => {
                     alert(errorMsg);
                 });
             },
 
-            fetchDataPoints(long_min, lat_min, long_max, lat_max) {
-                console.log(long_min, lat_min, long_max, lat_max);
-                return;
+            fetchDataPoints(heatmap, long_min, lat_min, long_max, lat_max) {
+                var heatmapData = [];
+
+                // fake the bound of the map.
+                long_min=-74.5;
+                lat_min=40.0;
+                long_max=-74.0;
+                lat_max=40.5;
 
                 this.$http.get(`/api/feedback?longitude_min=${long_min}&latitude_min=${lat_min}&longitude_max=${long_max}&latitude_max=${lat_max}`).then((res) => {
-                    console.log(res.json);
-                });
 
-                // _.each(apiData, (item) => {
-                //     this.heatmapData.push(new google.maps.LatLng(item.long, item.lat));
-                // });
+                    _.each(res.json(), (item) => {
+                        heatmapData.push(new google.maps.LatLng(item.lat, item.lon));
+                    });
+
+                    heatmap.setData(heatmapData);
+                });
             }
         },
 
